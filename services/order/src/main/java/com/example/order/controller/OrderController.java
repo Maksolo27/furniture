50164c0d9c10047baa5d3acb1b551502f515ab4b
Шухман Вероника <nikshuhman@gmail.com>
2023-03-30 08:38:27 -0700

    fix&added lab2

--- services/order/src/main/java/com/example/order/controller/OrderController.java
+++ services/order/src/main/java/com/example/order/controller/OrderController.java
@@ -2,30 +2,55 @@ package com.example.order.controller;
 
 import com.example.order.entities.Order;
 import com.example.order.services.OrderService;
-import lombok.AllArgsConstructor;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/order")
-@AllArgsConstructor
 public class OrderController {
 
-    private final OrderService orderService;
+    @Autowired
+    OrderService orderService;
+
+    @PostMapping("create")
+    public void createOrder (@RequestBody String json) {
+        Gson gson = new Gson ();
+        Order order = gson.fromJson (json, Order.class);
+        orderService.addOrder(order);
+    }
 
     @GetMapping()
-    public ResponseEntity<List<Order>> getAllOrder(){
-        return ResponseEntity.ok( orderService.getAllOrder());
+    public ResponseEntity<List<Order>> getAllOrders () {
+        System.out.println (orderService.getAllOrders());
+        return ResponseEntity.ok (orderService.getAllOrders());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Order> getAllOrder(@PathVariable Integer id){
-        return ResponseEntity.ok(orderService.getById(id));
+    public ResponseEntity<Order> getOrderById (@PathVariable String id) {
+        return ResponseEntity.ok (orderService.getAllOrders().get(Integer.parseInt(id)));
+    }
+
+    @PostMapping("update/{id}")
+    public void updateOrder (@PathVariable String id, @RequestBody String json) {
+        Gson gson = new Gson ();
+        Order order = gson.fromJson (json, Order.class);
+        Order chosenOrder = orderService.getAllOrders().get(Integer.parseInt(id));
+        chosenOrder.setCustomerName(order.getCustomerName());
+        chosenOrder.setItemName(order.getItemName());
+        chosenOrder.setItemPrice(order.getItemPrice());
+        chosenOrder.setQuantity(order.getQuantity());
+        orderService.updateOrder(order);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public void deleteOrder (@PathVariable String id) {
+        orderService.deleteById(Math.toIntExact(Long.valueOf(id)));
     }
 }
+
+
 
--- services/order/src/main/java/com/example/order/services/OrderService.java
+++ services/order/src/main/java/com/example/order/services/OrderService.java
@@ -3,21 +3,41 @@ package com.example.order.services;
 import com.example.order.entities.Order;
 import com.example.order.repository.OrderRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class OrderService {
 
-    private final OrderRepository orderRepository = new OrderRepository();
+    private final OrderRepository orderRepository;
 
     public List<Order> getAllOrder () {
         return orderRepository.findAll();
     }
 
-    public Order getById(Integer id){
+    public Optional<Order> getById(Integer id){
         return orderRepository.findById(id);
     }
+
+    public void addOrder(Order order) {
+        orderRepository.save(order);
+    }
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public void updateOrder(Order order) {
+        orderRepository.save(order);
+    }
+
+    public void deleteById(Integer id) {
+        orderRepository.deleteById(id);
+    }
 }
+
+
