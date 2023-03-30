7c3fedda0a1017d22411dad8992dd561d4ded711
Шухман Вероника <nikshuhman@gmail.com>
2023-03-30 05:44:49 -0700

    Revert "added lab2"
    This reverts commit 32782790c60cebcad7f238b4e98b15fef6d0146a.

--- services/order/src/main/java/com/example/order/controller/OrderController.java
+++ services/order/src/main/java/com/example/order/controller/OrderController.java
@@ -1,56 +1,31 @@
-package com.example.order.controllers;
+package com.example.order.controller;
 
 import com.example.order.entities.Order;
 import com.example.order.services.OrderService;
-import com.google.gson.Gson;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/order")
+@AllArgsConstructor
 public class OrderController {
 
-    @Autowired
-    OrderService orderService;
-
-    @PostMapping("create")
-    public void createOrder (@RequestBody String json) {
-        Gson gson = new Gson ();
-        Order order = gson.fromJson (json, Order.class);
-        orderService.addOrder(order);
-    }
+    private final OrderService orderService;
 
     @GetMapping()
-    public ResponseEntity<List<Order>> getAllOrders () {
-        System.out.println (orderService.getAllOrders());
-        return ResponseEntity.ok (orderService.getAllOrders());
+    public ResponseEntity<List<Order>> getAllOrder(){
+        return ResponseEntity.ok( orderService.getAllOrder());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Order> getOrderById (@PathVariable String id) {
-        return ResponseEntity.ok (orderService.getAllOrders().get(Integer.parseInt(id)));
+    public ResponseEntity<Order> getAllOrder(@PathVariable Integer id){
+        return ResponseEntity.ok(orderService.getById(id));
     }
-
-    @PostMapping("update/{id}")
-    public void updateOrder (@PathVariable String id, @RequestBody String json) {
-        Gson gson = new Gson ();
-        Order order = gson.fromJson (json, Order.class);
-        Order chosenOrder = orderService.getAllOrders().get(Integer.parseInt(id));
-        chosenOrder.setCustomerName(order.getCustomerName());
-        chosenOrder.setItemName(order.getItemName());
-        chosenOrder.setItemPrice(order.getItemPrice());
-        chosenOrder.setQuantity(order.getQuantity());
-        orderService.updateOrder(order);
-    }
-
-    @DeleteMapping("delete/{id}")
-    public void deleteOrder (@PathVariable String id) {
-        orderService.deleteById(Long.valueOf(id));
-    }
-}
-
 }
 
--- services/order/src/main/java/com/example/order/entities/Order.java
+++ services/order/src/main/java/com/example/order/entities/Order.java
@@ -5,28 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
-
-@Entity
-@Table(name = "orders")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Order {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer orderId;
-
-    @Column(name = "customer_name")
+    private int orderId;
     private String customerName;
-
-    @Column(name = "item_name")
     private String itemName;
-
-    @Column(name = "item_price")
-    private Double itemPrice;
-
-    @Column(name = "item_quantity")
-    private Integer quantity;
+    private double itemPrice;
+    private int quantity;
 }
