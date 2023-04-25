b1593712f1e63de16f09345f81d04122c3ed8dd2
maxim <makkov2705@gmail.com>
2023-04-25 19:45:52 +0300

    (fix) Fix helm

--- docker.sh
+++ docker.sh
@@ -1,10 +1,10 @@
 eval $(minikube docker-env)
 #docker build -t storage -f services/storage/Dockerfile services/storage
-docker build -t storage:2.0 -f services/storage/Dockerfile services/storage
-docker build -t storage-migrations:2.0 -f services/storage/Dockerfile.migration services/storage
-docker build -t order:2.0 -f services/order/Dockerfile services/order/
-docker build -t order-migrations:2.0 -f services/order/Dockerfile.migration services/order
-docker build -t buyer:2.0 -f services/buyer/Dockerfile services/buyer/
-docker build -t buyer-migrations:2.0 -f services/buyer/Dockerfile.migration services/buyer
-docker build -t client -f client/Dockerfile client
+docker build -t storage:25 -f services/storage/Dockerfile services/storage
+docker build -t storage-migrations:25 -f services/storage/Dockerfile.migration services/storage
+docker build -t order:25 -f services/order/Dockerfile services/order/
+docker build -t order-migrations:25 -f services/order/Dockerfile.migration services/order
+docker build -t buyer:25 -f services/buyer/Dockerfile services/buyer/
+docker build -t buyer-migrations:25 -f services/buyer/Dockerfile.migration services/buyer
+docker build -t client:25 -f client/Dockerfile client
 
